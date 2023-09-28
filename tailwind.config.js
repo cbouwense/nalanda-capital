31b1b1f2dbd7470899968fcd363df8667a30d13e
Christian Bouwense <christian@bigeye.com>
2023-09-28 11:35:33 -0400

    added all the portfolio sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,12 @@ module.exports = {
     extend: {
       colors: {
         "light-blue": "#13487d",
-        "dark-blue": "#022a52",
+        "dark-blue": "#0D285F",
         "dark-slate": "#373d45",
         tan: "#ece1c8",
         "dark-tan": "#ecb782",
         "light-black": "#2c2825",
+        rust: "#C83A26",
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
